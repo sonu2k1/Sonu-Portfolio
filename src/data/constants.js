d5d7f2763bed2d5031e5d4a67767a182102349c8
Sonu Singh <160756068+sonu2k1@users.noreply.github.com>
2025-05-16 10:31:14 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sonu2k1",
   resume:
-    "https://drive.google.com/file/d/1QMOpBF1N2LuV2zWY2nSHK76htW7K16kK/view?usp=sharing",
+    "https://drive.google.com/file/d/1TW-Rr1Vfw4jz90xovoKlOUzTqcyWdGGN/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/sonu-singh-429a86291/",
 
   insta: "https://www.instagram.com/sonu_singh_2k1/?hl=en",
